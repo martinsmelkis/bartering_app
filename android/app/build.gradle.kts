2ac0ffeba3f834104608efc5381c843ee7e63424
Martins Melkis <ajezi9@gmail.com>
2026-05-26 22:27:55 +0300

    Update app versions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "app.bartering.bartering_app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20
-        versionName = "0.2.0"
+        versionCode = 21
+        versionName = "0.2.1"
     }
 
     flavorDimensions += "environment"
